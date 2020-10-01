cf362aa931d5871a73a48ef1ced83ee043e76712
filippoferrando <ferrandodfilippo@gmail.com>
2020-10-01 22:52:01 +0200

    prettier

--- gioco.c
+++ gioco.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>   
+#include <time.h>
 #include <string.h>
 
 //betorglio e fenoglio
@@ -30,54 +30,60 @@ menù
 #define armatura_min 2
 #define LUNG 100
 #define n_mostri 2
- 
 
-typedef struct mostro{
+typedef struct mostro
+{
     int vita;
     int forza;
     int arma;
     int armatura;
     char nome[LUNG];
-}Mostro;
+} Mostro;
 
-void setArma(Mostro *m){
+void setArma(Mostro *m)
+{
     srand(time(NULL));
-    int arma = rand()%(arma_max-arma_min+1)+arma_min;
+    int arma = rand() % (arma_max - arma_min + 1) + arma_min;
 
     m->arma = arma;
 }
 
-void setArmatura(Mostro *m){
+void setArmatura(Mostro *m)
+{
     srand(time(NULL));
-    int armatura = rand()%(armatura_max-armatura_min+1)+armatura_min;
+    int armatura = rand() % (armatura_max - armatura_min + 1) + armatura_min;
 
     m->armatura = armatura;
 }
 
-void setVita(int vita, Mostro *m){
-    if(vita > vitaMin || vita < vitaMax){
+void setVita(int vita, Mostro *m)
+{
+    if (vita > vitaMin || vita < vitaMax)
+    {
         m->vita = vita;
     }
 }
 
-void setForza(Mostro *m){
+void setForza(Mostro *m)
+{
     srand(time(NULL));
-    int forza = rand()%(forza_max-forza_min+1)+forza_min;
+    int forza = rand() % (forza_max - forza_min + 1) + forza_min;
 
     m->forza = forza;
 }
 
-
-
-int getVita(Mostro *m){
+int getVita(Mostro *m)
+{
     return m->vita;
 }
 
-int getForza(Mostro *m){
+int getForza(Mostro *m)
+{
     return m->forza;
 }
 
-void toString(Mostro m){ //visualizza struttura mostro
+void toString(Mostro m)
+{ //visualizza struttura mostro
     printf("Nome = %s\n", &(m.nome));
     printf("Vita = %d\n", m.vita);
     printf("Forza = %d\n", m.forza);
@@ -85,94 +91,110 @@ void toString(Mostro m){ //visualizza struttura mostro
     printf("Armatura = %d\n", m.armatura);
 }
 
-int menu(){
+int menu()
+{
     int n;
     printf("Premere 0 per uscire\n");
     printf("Premere 1 per creare il personaggio\n");
     printf("Premere 2 per giocare\n");
     fflush(stdin);
-    scanf("%d",&n);
+    scanf("%d", &n);
     return n;
 }
 
-
-Mostro creaMostro(char nome[LUNG]){
+Mostro creaMostro(char nome[LUNG])
+{
     Mostro m;
     setVita(vitaIniziale, &m);
     setForza(&m);
     setArma(&m);
     setArmatura(&m);
     strcpy(m.nome, nome);
-    
+
     m.forza = m.forza + m.arma;
     m.vita = m.armatura + m.vita;
 
     return m;
 }
 
-void main(){
+void main()
+{
     Mostro n1, n2;
     int opz;
     char comandoAttacco;
     int numeroMostri = 0;
     char nome[LUNG];
 
-    do{
-        opz=menu();
-        switch(opz){
-            case 0:
-                printf("Hai scelto si uscire");
+    do
+    {
+        opz = menu();
+        switch (opz)
+        {
+        case 0:
+            printf("Hai scelto si uscire");
             break;
-            case 1:
-                   
-                if (numeroMostri < 1) {
+        case 1:
+
+            if (numeroMostri < 1)
+            {
+                numeroMostri++;
+                fflush(stdin);
+                scanf("%s", &nome);
+                n1 = creaMostro(nome);
+                toString(n1);
+                printf("\n");
+            }
+            else
+            {
+                if (numeroMostri < 2)
+                {
                     numeroMostri++;
                     fflush(stdin);
                     scanf("%s", &nome);
-                    n1 = creaMostro(nome);
-                    toString(n1);
+                    n2 = creaMostro(nome);
+                    toString(n2);
                     printf("\n");
-                }else{
-                    if(numeroMostri < 2){
-                        numeroMostri++;
-                        fflush(stdin);
-                        scanf("%s", &nome);
-                        n2 = creaMostro(nome);
-                        toString(n2);
-                        printf("\n");
-                    }else{
-                        printf("\nLimite mostri raggiunto\n");
-                    }
                 }
+                else
+                {
+                    printf("\nLimite mostri raggiunto\n");
+                }
+            }
             break;
 
-            case 2:
-                printf("1 giocare preme F per giocare\n2 giocatore preme B per giocare\n");
-                do{
-                    scanf("%c", &comandoAttacco);
-                    if(comandoAttacco == 'f' || comandoAttacco == 'F'){
-                        printf("%s ATTACCA %s\n", &(n1.nome), &(n2.nome));
-                        n2.vita -= n1.forza;
-                        printf("Vita attuale di %s = %d\n", &(n2.nome), n2.vita);
-                    } else if(comandoAttacco == 'b' || comandoAttacco == 'B'){
-                        printf("%s ATTACCA %s\n", &(n2.nome), &(n1.nome));  
-                        n1.vita = n1.vita - n2.forza;
-                        printf("Vita rimanente di %s = %d\n", &(n1.nome), n1.vita);
-                    }
-                } while(n1.vita > 0 && n2.vita > 0);
-
-                printf("\n\n");
-                
-                if(n1.vita>n2.vita){
-                    printf("%s HA VINTO!", n1.nome);
-                }else{
-                    printf("%s HA VINTO!", n2.nome);
+        case 2:
+            printf("1 giocare preme F per giocare\n2 giocatore preme B per giocare\n");
+            do
+            {
+                scanf("%c", &comandoAttacco);
+                if (comandoAttacco == 'f' || comandoAttacco == 'F')
+                {
+                    printf("%s ATTACCA %s\n", &(n1.nome), &(n2.nome));
+                    n2.vita -= n1.forza;
+                    printf("Vita attuale di %s = %d\n", &(n2.nome), n2.vita);
                 }
+                else if (comandoAttacco == 'b' || comandoAttacco == 'B')
+                {
+                    printf("%s ATTACCA %s\n", &(n2.nome), &(n1.nome));
+                    n1.vita = n1.vita - n2.forza;
+                    printf("Vita rimanente di %s = %d\n", &(n1.nome), n1.vita);
+                }
+            } while (n1.vita > 0 && n2.vita > 0);
 
-                printf("\n\n");
+            printf("\n\n");
+
+            if (n1.vita > n2.vita)
+            {
+                printf("%s HA VINTO!", n1.nome);
+            }
+            else
+            {
+                printf("%s HA VINTO!", n2.nome);
+            }
+
+            printf("\n\n");
 
             break;
-
         }
-    } while(opz!=0);
+    } while (opz != 0);
 }
